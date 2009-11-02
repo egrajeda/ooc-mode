e3c80835a53c343ba2dd895ff7ae00c39b21ca1f
Eduardo Grajeda <tatofoo@gmail.com>
2009-11-01 18:08:56 -0600

    When indenting, the cursor no longers go to the beginning of the line, now it stays in its original position

--- ooc-mode.el
+++ ooc-mode.el
@@ -71,38 +71,41 @@
 (defvar ooc-font-lock-keywords ooc-font-lock-keywords-2
   "Default highlighting expressions for OOC mode")
 
-;; no special indenting, just pure text mode
+;; some basic indenting rules
 (defun ooc-indent-line ()
   "Indent current line as OOC code."
   (interactive)
-  (beginning-of-line)
-  (if (bobp)
-      (indent-line-to 0)
-    (let ((not-indented t) cur-indent)
-      (if (looking-at "^[ \t]*}")
-          (progn
-            (save-excursion
+  (let ((cur-pos (point)) pos-in-line)
+    (beginning-of-line)
+    (setq pos-in-line (- cur-pos (point)))
+    (if (bobp)
+        (indent-line-to 0)
+      (let ((not-indented t) cur-indent)
+        (if (looking-at "^[ \t]*}")
+            (progn
+              (save-excursion
+                (forward-line -1)
+                (setq cur-indent (- (current-indentation) default-tab-width)))
+              (if (< cur-indent 0)
+                  (setq cur-indent 0)))
+          (save-excursion
+            (while not-indented
               (forward-line -1)
-              (setq cur-indent (- (current-indentation) default-tab-width)))
-            (if (< cur-indent 0)
-                (setq cur-indent 0)))
-        (save-excursion
-          (while not-indented
-            (forward-line -1)
-            (if (looking-at "^[ \t]*}")
-                (progn
-                  (setq cur-indent (current-indentation))
-                  (setq not-indented nil))
-              (if (looking-at ".*{")
+              (if (looking-at "^[ \t]*}")
                   (progn
-                    (setq cur-indent (+ (current-indentation) default-tab-width))
+                    (setq cur-indent (current-indentation))
                     (setq not-indented nil))
-                (if (bobp)
-                    (setq not-indented nil)))))))
-      (if cur-indent
-          (indent-line-to cur-indent)
-        (indent-line-to 0)))))
-
+                (if (looking-at ".*{")
+                    (progn
+                      (setq cur-indent (+ (current-indentation) default-tab-width))
+                      (setq not-indented nil))
+                  (if (bobp)
+                      (setq not-indented nil)))))))
+        (if cur-indent
+            (indent-line-to cur-indent)
+          (indent-line-to 0))))
+    (forward-char pos-in-line)))
+  
 ;; no special syntax table
 (defvar ooc-mode-syntax-table nil
   "Syntax table for ooc-mode.")
